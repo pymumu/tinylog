452af07c8ff1d39a47d2076fbe304d18184acfa7
Nick Peng <pymumu@gmail.com>
2018-03-21 00:01:52 +0800

    update code.

--- example/example.c
+++ example/example.c
@@ -6,4 +6,5 @@ int main(int argc, char *argv[])
     tlog_init("./", "example.log", 1024 * 1024, 8, 1, 0);
     tlog(TLOG_INFO, "This is a log message.\n");
     tlog_exit();
+    return 0;
 }
--- tlog.h
+++ tlog.h
@@ -36,16 +36,39 @@ struct tlog_info {
     struct tlog_time time;
 };
 
+/*
+Function：Print log   
+level: Current log Levels  
+format: Log formats  
+*/
 #define tlog(level, format, ...) tlog_ext(level, __FILE__, __LINE__, __func__, 0, format, ##__VA_ARGS__)
 
-extern int tlog_vext(tlog_level level, const char *file, int line, const char *func, void *userptr, const char *format, va_list ap);
+extern int tlog_ext(tlog_level level, const char *file, int line, const char *func, void *userptr, const char *format, ...) __attribute__((format(printf, 6, 7)));
 
-extern int tlog_ext(tlog_level level, const char *file, int line, const char *func, void *userptr, const char *format, ...);
+/* set log level */
+extern int tlog_setlevel(tlog_level level);
 
+/*
+Function：Initialize log module  
+logdir: Log Output path.    
+logname: Log name.  
+maxlogsize: The maximum size of a single log file.    
+maxlogcount: Number of archived logs.    
+block: Blocked if buffer is not sufficient.    
+buffsize: Buffer size  
+ */
 extern int tlog_init(const char *logdir, const char *logname, int maxlogsize, int maxlogcount, int block, int buffsize);
 
 extern void tlog_exit(void);
 
+/*
+customize log output format
+steps:
+1. define format function, function must be defined as tlog_format_func, use snprintf or vsnprintf format log to buffer
+2. call tlog_reg_format_func to register format function.
+
+read _tlog_format for example.
+*/
 typedef int (*tlog_format_func)(char *buff, int maxlen, struct tlog_info *info, void *userptr, const char *format, va_list ap);
 extern int tlog_reg_format_func(tlog_format_func func);
 
